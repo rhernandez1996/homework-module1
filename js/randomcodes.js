663b49505f69ba7acb5543454bda810f628e03a2
rhernandez1996 <rayvenhernandez14@yahoo.com>
2022-09-25 10:08:19 -0700

    added btn value

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,16 +1,18 @@
 /* -------Random Codes---------*/
-
+ var code = ""; //initialize to null value
+ var getCode = ""; //store entered code
+ var btnvalue; //for button Boolean
+ //create variable to hold the type of characters we want to show as codes
+ var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 //Function to generate combination of characters
 
 function generateCode() {
     //Create variables to store generated codes and the type of characters we want to show as codes
-    var code = ""; //initialize to null value
-    var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
-
+    
     //Generate character multiple times using a loop
     for (i = 1; i<= 8; i++) {
       var char = Math.random()* str.length; //random select a character from the variable and then store in a new variable
-      code. += str.charAt(char); //accumulate the generated character into a string of 8 characters
+      code += str.charAt(char); //accumulate the generated character into a string of 8 characters
   }
   return code; //return the final accumulated string when the loop ends
 }
@@ -19,8 +21,13 @@ function generateCode() {
 document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton() {
-  document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+  document.getElementById("submit").disabled = btnvalue; //able/disable button
+  if (btnvalue == true) { //test if button is disableed or enabled
+    //set button and label color translucent
+    document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+    document.getElementById("submit").style.color = "rgba(25, 255, 255, 0.5)";
+    }
 }
 //Activate function
 disableButton();
