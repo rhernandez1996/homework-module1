eab5ca36050cf229db89e5f06f64b49490b1dc1d
rhernandez1996 <rayvenhernandez14@yahoo.com>
2022-10-04 08:31:11 -0700

    table stuff

--- js/activity.js
+++ js/activity.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-$('table').bind("click", function(e) {
-$(e.target).closest("tr").toggleClass("highlight");
-});    
+$(document).ready(function () {
+  $("td").on("click", function () {
+    //select data cell
+    var content = $(this).text();
+
+    if (content != "Not Avaliable") {
+      $(this).toggleClass("tdhighlight"); //add or remove cell when selected
+
+      if ($(this).hasClass("tdhighlight")) {
+        //check if selected cell has class
+        $("displaySelected").css("visibility", "visible"); //make display box visible
+        $("displaySelected").css("margin-top", "2em"); //add space above display box
+        $("#result").append("<p>" + content + "</p>"); //add child element with contents of cell
+      } else {
+        //if selectedd cell dont have class
+        $("#result p:contains(" + content + ")").remove(); //remove child element
+
+        if ($("#result").has("p").length == false) {
+          //checkif there are any child elements within parent
+          $("displaySelected").css("visibility", "hidden"); //make display box hidden
+          $("displaySelected").css("margin-top", "0"); //remove spaces above display box
+        }
+      }
+    }
+  });
 });
